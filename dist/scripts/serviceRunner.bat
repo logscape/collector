558fd9f3c52a26382d26b52f955f631ed530b6b0
Nathen <nathen.sample@logscape.com>
2015-09-25 17:22:04 +0100

    dist folder cleanup

--- dist/scripts/serviceRunner.bat
+++
@@ -1,6 +0,0 @@
-
-set CP=".;./lib/*"
-echo "Running with CP:$CP $1"
-set MAIN=com.liquidlabs.vso.deployment.ServiceTestHarness
-echo "%JAVA_HOME%"\bin\java.exe -cp %CP% %MAIN% %*
-"%JAVA_HOME%"\bin\java.exe -cp "%CP%" %MAIN% %*
